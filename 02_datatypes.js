0de349fe396869bbce0d4d4a825b43e4ef23b465
Bishal Kumar Sah <bishalkumarsah000@gmail.com>
2025-06-27 16:32:24 +0530

    Create 02_datatypes.js

---
+++ 02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  //treat all js code as newer version
+
+// alert(3+3)  // WE are using node js , not browser
+
+console.log(3+3)
+console.log("Bishal")  //code readability should be high
+
+let name = "Bishal"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "Bishal");
+console.log(typeof null);
